ee80b85e05e0bb4a1be89c739f29b6000d126f47
irminaj <irminaj@github.com>
2022-05-11 22:39:54 +0300

    Finished eighth task

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,23 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+  this.a = a;
+  this.b = b;
+  this.sum = function () {
+    return this.a + this.b;
+  };
+  this.substract = function () {
+    return this.a - this.b;
+  };
+  this.multiply = function () {
+    return this.a * this.b;
+  };
+  this.divide = function () {
+    return this.a / this.b;
+  };
+}
+
+const newNumbers = new Calculator(10, 5);
+console.log(newNumbers.divide());
